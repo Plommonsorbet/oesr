3b89e4495edbccef392fb0ff039a1f2f01b452de
Isak Johansson <isak@irq1.se>
2021-08-16 20:10:33 +0200

    Clean up live-cd

--- live-cd/live-cd.nix
+++ live-cd/live-cd.nix
@@ -1,15 +1,14 @@
 { pkgs, config, ... }: {
-  services.pcscd.enable = true;
-  services.udev.packages = with pkgs; [ yubikey-personalization ];
+  #services.pcscd.enable = true;
+  #services.udev.packages = with pkgs; [ yubikey-personalization ];
 
   environment.systemPackages = with pkgs;
     [ gnupg pinentry-curses haskellPackages.hopenpgp-tools oesr.sss-cli oesr.oesr-cli ];
 
-  programs = {
-    ssh.startAgent = false;
-    gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true;
-    };
-  };
+  #programs = {
+  #  ssh.startAgent = false;
+  #  gnupg.agent = {
+  #    enable = true;
+  #  };
+  #};
 }
